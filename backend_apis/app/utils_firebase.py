13ae59f214cce9ce9a42a2cd4c1663ba2620756a
somanishivam <32792568+somanishivam@users.noreply.github.com>
2023-11-06 18:06:40 +0530

    Adding Auth + Marketing Campaign APIs (#5)
    * Adding API with SecretManager
    
    * Adding APIs
    
    * Adding APIs
    
    * Campaing API with brief content generation stored in google doc
    
    * Config File Update
    
    * Config File Update
    
    * Readme File Update
    
    * Token auth, translation api, and content creation api added
    
    * Token auth, translation api, and content creation api added

---
+++ backend_apis/app/utils_firebase.py
@@ -0,0 +1,105 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""
+Utility module for Firestore.
+"""
+
+import firebase_admin
+from firebase_admin import firestore,credentials, auth
+import os
+from .body_schema import Campaign, CampaignList
+import json
+from google.cloud import secretmanager
+import tomllib
+import google.oauth2.id_token
+import google.auth.transport.requests
+
+#Auth Request
+HTTP_REQUEST = google.auth.transport.requests.Request()
+
+# Load configuration file
+with open("/code/app/config.toml", "rb") as f:
+    config = tomllib.load(f)
+
+
+# Fetch Secret Configuration
+secret_client = secretmanager.SecretManagerServiceClient()
+secret_name = config['global']['secret_name']
+secret_response = secret_client.access_secret_version(name=secret_name)
+firebaseConfig = secret_response.payload.data.decode("UTF-8")
+
+firebaseConfig = json.loads(firebaseConfig)
+
+# Application Default credentials are automatically created.
+app = firebase_admin.initialize_app()
+db = firestore.client()
+
+def to_serializable(val):
+    if hasattr(val, '__dict__'):
+        return val.__dict__
+    return val
+
+def verify_auth_token(id_token):
+    # Verify Firebase auth.
+    claims = google.oauth2.id_token.verify_firebase_token(
+        id_token, HTTP_REQUEST, audience=os.environ.get("GOOGLE_CLOUD_PROJECT")
+    )
+    if not claims:
+        return "000"
+    
+    user_id = claims['sub']
+    friendly_id = claims.get("name", claims.get("email", "Unknown"))
+    print(friendly_id)
+    return user_id
+
+def create_campaign(user_id,campaign:Campaign):
+    user_ref = db.collection("users").document(user_id)
+    if user_ref.get().exists:
+        print("Found user")
+    print(json.dumps(campaign.__dict__,default=to_serializable))
+    update_time, campaign_ref = user_ref.collection("campaigns").add(json.loads(json.dumps(campaign.__dict__,default=to_serializable)))
+
+    return update_time,campaign_ref.id
+
+
+def read_campaign(user_id,campaignid):
+    campaign_ref = db.collection("users").document(user_id).collection("campaigns").document(campaignid)
+    return campaign_ref.to_dict()
+
+def list_campaigns(user_id):
+    campaign_col = db.collection("users").document(user_id).collection("campaigns").stream()
+    list_campaigns = []
+    for campaign in campaign_col:
+        camp_dict = campaign.to_dict()
+        print(camp_dict)
+        list_campaigns.append(CampaignList(id=campaign.id,data=Campaign(**camp_dict)))
+    return list_campaigns
+
+def update_campaign(user_id,campaign_id,data:Campaign):
+    camp_ref = db.collection("users").document(user_id).collection("campaigns").document(campaign_id)
+    if camp_ref.get().exists:
+        print("Updating Campaign")
+        camp_ref.set(json.loads(json.dumps(data.__dict__,default=to_serializable)))
+        return f"{campaign_id} Campaign is updated."
+    
+def delete_campaign(user_id,campaign_id):
+    camp_ref = db.collection("users").document(user_id).collection("campaigns").document(campaign_id)
+    if camp_ref.get().exists:
+        print("Deleting Campaign")
+        camp_ref.delete()
+        return f"{campaign_id} Campaign is deleted."
+    return f"{campaign_id} Campaign does not exists."
+
---
+++ backend_apis/app/utils_prompt.py
@@ -0,0 +1,89 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""
+A utility module for performing prompt engineering with the Vertex PaLM API.
+"""
+
+
+import asyncio
+import functools
+import vertexai
+import tomllib
+
+# Load configuration file
+with open("/code/app/config.toml", "rb") as f:
+    config = tomllib.load(f)
+project_id = config["global"]["project_id"]
+location = config["global"]["location"]
+
+vertexai.init(project=project_id, location=location)
+
+from vertexai.preview.language_models import TextGenerationModel
+from vertexai.preview.vision_models import ImageGenerationModel
+
+async def async_predict_text_llm(
+        prompt: str,
+        pretrained_model: str,
+        max_output_tokens: int=1024,
+        temperature: float=0.4,
+        top_k: int=40,
+        top_p: float=0.8
+    )-> str:
+    loop = asyncio.get_running_loop()
+    llm = TextGenerationModel.from_pretrained(pretrained_model)
+    generated_response = None
+   
+    try:
+        generated_response = await loop.run_in_executor(
+            None,
+            functools.partial(
+                llm.predict,
+                    prompt=prompt, 
+                    temperature=temperature, 
+                    max_output_tokens=max_output_tokens, 
+                    top_k=top_k, top_p=top_p))
+    except Exception as e:
+        print(e)
+
+    if generated_response and generated_response.text:
+        return generated_response.text
+    return ""
+
+async def async_generate_image(prompt,number_of_images=4):
+    loop = asyncio.get_running_loop()
+    # Image models
+    imagen = ImageGenerationModel.from_pretrained("imagegeneration@002")
+    try:
+        imagen_responses = await loop.run_in_executor(
+            None,
+            functools.partial(
+                imagen.generate_images,
+                    prompt=prompt, 
+                    number_of_images=number_of_images))
+    except Exception as e:
+        print(str(e))
+    else:
+        generated_images = []
+        for image in imagen_responses:
+            generated_images.append(
+                {
+                    "images_base64_string": image._as_base64_string(),
+                    "image_size": image._size,
+                    "images_parameters": image.generation_parameters
+                }
+            )
+        return generated_images
+
