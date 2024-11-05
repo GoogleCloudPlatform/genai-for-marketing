bcd3922c7a74fbb24b5e60c6941bda5c9119ae1f
Andrea Cervesato <andrea.cervesato@gmail.com>
2024-11-05 13:47:38 +0100

    wrong apices

--- infra/main.tf
+++ infra/main.tf
@@ -34,7 +34,7 @@ resource "null_resource" "py_venv" {
 
   provisioner "local-exec" {
     working_dir = "scripts/"
-    command     = "if [ ! -d "venv" ]; then python3 -m venv venv;fi; venv/bin/pip install -r requirements.txt"
+    command     = "if [ ! -d 'venv' ]; then python3 -m venv venv;fi; venv/bin/pip install -r requirements.txt"
   }
 
 }
