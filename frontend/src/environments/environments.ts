71176318bed3db669ce418aec4fa4db0e00cd39e
Andrea Cervesato <andrea.cervesato@gmail.com>
2024-11-20 13:48:55 +0100

    Restore point

--- frontend/src/environments/environments.ts
+++ frontend/src/environments/environments.ts
@@ -3,12 +3,12 @@ export const environment = {
     apiUrl: "https://genai-for-marketing-backend-apis-jna3mzxufa-uc.a.run.app/marketing-api",
     dialogflowCxAgentId: "5249b9ad-8f3f-4e12-9838-6da80368e992",
     firebaseConfig: {
-        apiKey: "AIzaSyB8BbumufKiBsXMnqUxYfPAitB6OpkkQHs",
+        apiKey: "AIzaSyA_OTLBdkIz280izogiWmLps4LJuY2Thmw",
         authDomain: "genai-for-marketing-3.firebaseapp.com",
         projectId: "genai-for-marketing-3",
         storageBucket: "genai-for-marketing-3.firebasestorage.app",
         messagingSenderId: "124117379078",
-        appId: "1:124117379078:web:cd5e5d9542c5f195e7bb87",
+        appId: "1:124117379078:web:7b16f4a42739b68de7bb87",
         measurementId: ""
       }
 }
